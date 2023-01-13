77032e1e738d80680d94ffc4a8828ef141b41ea1
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2023-01-12 20:53:06 -0500

    added credentials

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: HASHMEET SINGH SAINI    Student ID: 153070214   Date: 12 January 2023
+*  Cyclic Link: https://magenta-millipede-cuff.cyclic.app
+*
+********************************************************************************/ 
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
